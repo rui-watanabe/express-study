9e77b6e827907c58f568267fa0743e3d03404c10
rui-watanabe <wataru610806@gmail.com>
2020-03-15 10:34:24 +0900

    change_function box

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,15 @@
 'use strict';
 import dc from 'damage-calc';
+import $ from 'jquery';
+
 const root = document.getElementById('root');
 root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
   + dc.effectiveDamage(100, 50, 30) + '</p>';
+
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
